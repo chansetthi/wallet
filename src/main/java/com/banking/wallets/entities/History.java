72a5fcfe2a8f0e128f3cf6c026166cd001d57c1b
chansetthi <chansetthi@gmail.com>
2020-01-26 15:59:09 +0700

    delete string test

--- src/main/java/com/banking/wallets/entities/History.java
+++ src/main/java/com/banking/wallets/entities/History.java
@@ -13,7 +13,6 @@ public class History {
     private String payer;
     private String recelver;
     private String money;
-    private String tests2;
 
     public Long getId() {
         return id;
